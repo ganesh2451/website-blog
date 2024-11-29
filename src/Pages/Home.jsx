8b7ba5f4798daab33494630882460dd0f24e6471
ganesh2451 <ganeshami55555@gmail.com>
2024-11-29 19:37:55 +0530

    Blog website

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Blogs from '../components/Blogs'
+import Pagination from '../components/Pagination'
+import Header from '../components/Header'
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+        <div className='mt-30 flex w-full justify-center items-center'>
+            <Blogs />
+            <Pagination />
+        </div>
+    </div>
+  )
+}
+
+export default Home
